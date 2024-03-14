d09e0355be44f6d8461dbbcc2ccadd8fe5294dfa
Nick <NickyDaB@gmail.com>
2024-03-14 11:29:13 -0400

    Update hello_world.cs

--- hello_world.cs
+++ hello_world.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class hello_world : MonoBehaviour
 {
+
+    //Variables
+    string PlayerName = "Alex";
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello world!");
+    }
+
+    void PrintPlayerName()
+    {
+        Debug.Log("The Player's Name is: " + PlayerName);
     }
 
 
